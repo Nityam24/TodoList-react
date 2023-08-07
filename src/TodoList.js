5bd099bf5939883726b2473301131613b95c36eb
Nityam24 <nityampatel2402@gmail.com>
2023-08-07 22:36:37 +0530

    new update

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,6 +4,9 @@ export default function TodoList() {
   return (
     <main>
       <h1>Todo List</h1>
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear All</button>
     </main>
   );
 }
